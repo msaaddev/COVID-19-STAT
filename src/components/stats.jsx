03f232133c5e619991ae1cf9adf2bb13b8ee3f29
msaaddev <mrsaadirfan@gmail.com>
2020-03-30 14:19:26 +0500

    👌IMPROVE: search component

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,10 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
 class Stats extends Component {
-	state = {  }
-	render() { 
-		return (  );
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiData: []
+    };
+  }
+
+  componentWillMount = async () => {
+    try {
+      const response = await axios.get(`https://corona.lmao.ninja/countries`);
+      this.setState({ apiData: response.data });
+      console.log(this.state.apiData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    return <p>Hello</p>;
+  }
 }
- 
+
 export default Stats;
