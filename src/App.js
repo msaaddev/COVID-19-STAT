1fe1bfba62df38dfeeeaa0d9346a6fa694b18dd9
msaaddev <mrsaadirfan@gmail.com>
2020-04-03 01:26:21 +0500

    📦NEW: route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Data from "./components/data";
 import Nav from "./components/nav";
 import Growth from "./components/growth";
+import IndividualGrowth from "./components/individualcountrygrowth";
 import "./App.css";
 
 class App extends Component {
@@ -13,12 +14,15 @@ class App extends Component {
     super(props);
     this.state = {
       country: "",
+      countryCases: [],
+      countryDeaths: [],
+      countryRecovered: [],
       check: false
     };
   }
 
-  onSearch = country => {
-    this.setState({ country });
+  onSearch = (country, countryCases, countryDeaths, countryRecovered) => {
+    this.setState({ country, countryCases, countryDeaths, countryRecovered });
   };
 
   afterSearch = () => {
@@ -39,13 +43,33 @@ class App extends Component {
           <Route
             exact
             path="/"
-            render={() => <Data check={this.state.check} apiData="" />}
+            render={() => (
+              <Data
+                check={this.state.check}
+                apiData=""
+              />
+            )}
           />
           <Route
             exact
             path="/search"
             render={() => (
-              <Data check={this.state.check} apiData={this.state.country} />
+              <Data
+                check={this.state.check}
+                apiData={this.state.country}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/COVID-19-country-graphs"
+            render={() => (
+              <IndividualGrowth
+                cases={this.state.countryCases}
+                deaths={this.state.countryDeaths}
+                recovered={this.state.countryRecovered}
+                name={this.state.country.country}
+              />
             )}
           />
           <Route exact path="/growth" component={Growth} />
